2e2b838736eb03caeabb094eb7cf9e93ea9ed5e3
mhossain2 <moinulhossainmahim@gmail.com>
2024-09-04 11:09:31 +0600

    added listing edit functionality

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -136,7 +136,7 @@
               name="description"
               rows="10"
               placeholder="Include tasks, requirements, salary, etc"
-            	value="{{old('description')}}"
+            	{{old('description')}}
           ></textarea>
           @error('description')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
